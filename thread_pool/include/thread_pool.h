89bda9c4df6462500384285de61e706c67195dd0
James Lambert <james.lambert3995@gmail.com>
2023-03-02 16:47:27 -0500

    Adding a few tests and a size function to the queue

--- thread_pool/include/thread_pool.h
+++ thread_pool/include/thread_pool.h
@@ -7,9 +7,32 @@ extern "C"
 #endif
 #include "exit_codes.h"
     typedef struct thread_pool thread_pool_t;
-    thread_pool_t *tpool_init(int threads, void (*free_func)(void *));
-    exit_code_t shutdown(thread_pool_t *tpool);
+
+    /**
+     * @brief
+     *
+     * @param threads - number of threads to be made
+     * @param work_func - Function that each thread should perform
+     * @param free_func - Function for freeing the data for the jobs going in the work queue
+     * @return thread_pool_t*
+     */
+    thread_pool_t *tpool_init(int threads, void (*work_func)(void *));
+
+    /**
+     * @brief
+     *
+     * @param tpool
+     * @return exit_code_t
+     */
     exit_code_t tpool_destroy(thread_pool_t *tpool);
+
+    /**
+     * @brief
+     *
+     * @param tpool
+     * @param work
+     */
+    void tpool_add_work(thread_pool_t *tpool, void *work);
 
 #ifdef __cplusplus
 }
--- thread_pool/test/thread_pool_gtest.cpp
+++ thread_pool/test/thread_pool_gtest.cpp
@@ -1,6 +1,20 @@
 #include <gtest/gtest.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "thread_pool.h"
 #include "exit_codes.h"
+
+typedef struct temp
+{
+    int a;
+    int b;
+} temp_t;
+
+void add_stuff(void *temp_val)
+{
+    temp_t *temp = (temp_t *)temp_val;
+    printf("%d + %d = %d\n", temp->a, temp->b, temp->a + temp->b);
+}
 
 TEST(BaseTest, Initialize_and_Destroy_Func)
 {
@@ -8,4 +22,22 @@ TEST(BaseTest, Initialize_and_Destroy_Func)
     EXPECT_NE(tpool, nullptr);
     exit_code_t ret_val = tpool_destroy(tpool);
     EXPECT_EQ(ret_val, E_SUCCESS);
+}
+
+TEST(BaseTest, Add_Five_Jobs)
+{
+    temp_t *val = (temp_t *)calloc(1, sizeof(temp_t));
+    val->a = 2;
+    val->b = 5;
+    thread_pool_t *tpool = tpool_init(5, add_stuff);
+    tpool_add_work(tpool, val);
+    tpool_add_work(tpool, val);
+    tpool_add_work(tpool, val);
+    tpool_add_work(tpool, val);
+    tpool_add_work(tpool, val);
+    tpool_add_work(tpool, val);
+    tpool_add_work(tpool, val);
+    tpool_add_work(tpool, val);
+    tpool_destroy(tpool);
+    free(val);
 }
